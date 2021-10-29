e49a487d04c96f4bef94b42180fc6f117aca6e07
Sarang Rawat <sarangrawat05@gmail.com>
2021-10-29 19:49:41 +0530

    Added 'sortedArrayToBST' function.

--- Binary Tree/CreateBST.cpp
+++ Binary Tree/CreateBST.cpp
@@ -32,6 +32,15 @@ Node* BSTfromPreorder(int preorder[], int size) {
     int preorderIdx = 0;
     return BSTfromPreorder_util(preorder, size, &preorderIdx, preorder[0], INT_MIN, INT_MAX);
 }
+// Creates a BST from Sorted Array. O(n)
+Node* sortedArrayToBST(int arr[], int start, int end) {
+    if(start>end) return NULL;
+    int mid = start + (end-start)/2;
+    Node* root = new Node(arr[mid]);
+    root->left = sortedArrayToBST(arr, start, mid-1);
+    root->right = sortedArrayToBST(arr, mid+1, end);
+    return root;
+}
 // For Checking
 void inorder(Node* root) {
     if(root==NULL) return;
@@ -43,5 +52,11 @@ int main() {
     int p[6] = {8,5,1,7,10,12};
     Node* root1 = BSTfromPreorder(p, 6);
     inorder(root1);
+    cout<<endl;
+
+    int arr[] = {2,4,6,8,9,10,22};
+    Node* root2 = sortedArrayToBST(arr, 0, 6);
+    inorder(root2);
+    cout<<endl;
     return 0;
 }
