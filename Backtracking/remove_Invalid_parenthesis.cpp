c56e8c2ebc207da006426dce5c7430afb6e0cb95
Sarang Rawat <sarangrawat05@gmail.com>
2022-02-11 20:56:10 +0530

    Create remove_Invalid_parenthesis.cpp

---
+++ Backtracking/remove_Invalid_parenthesis.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+vector<string> res;
+unordered_map<string, bool> occurred;
+// Returns the no. of invalid parenthesis in a string
+int getMinInvalid(string data) {
+    stack<char> s;
+    for(auto x: data) {
+        if(x=='(' || x==')') {
+            if(s.empty()) {
+                s.push(x);
+            } else if(x==')' && s.top()=='(') {
+                s.pop();
+            } else {
+                s.push(x);
+            }
+        }
+    }
+    return s.size(); //stack would be left with only invalid parenthesis
+}
+
+void solve(string s, int minInv) {
+    // So that we don't process the same string again
+    if(occurred[s]==true) return;
+    else occurred[s] = true;
+    // base case
+    if(minInv<0) return;
+
+    // 
+    if(minInv==0) {
+        if(getMinInvalid(s)==0) {
+            res.push_back(s);
+        }
+        return;
+    }
+
+    for(int i=0; i<s.size(); i++) {
+        string left = s.substr(0, i);
+        string right = s.substr(i+1);
+        solve(left+right, minInv-1);
+    }
+
+}
+int main() {
+    string s = "()())()";
+    solve(s, getMinInvalid(s));
+//  ANS:  ["(())()","()()()"]
+    for(auto line: res) {
+        cout<<line<<endl;
+    }
+}
