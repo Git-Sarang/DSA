cc5ca059d3d03b82ae2f6fddfa139d70f2f502eb
Sarang Rawat <sarangrawat05@gmail.com>
2022-03-24 03:37:03 +0530

    Update bitwise_problems.cpp

--- Bit_Manipulation/bitwise_problems.cpp
+++ Bit_Manipulation/bitwise_problems.cpp
@@ -12,11 +12,19 @@ int numberOfones(int n) {
 
 void printSubsets(vector<char> &v) {
     int n = v.size();
-    
+    for(int i=0; i< (1<<n); i++) {
+        for(int j=0; j<n; j++) {
+            if( i & (1<<j)) {
+                cout<<v[j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
 }
 int main() {
     cout<<numberOfones(5)<<endl; // 5 = 101
     cout<<numberOfones(1524)<<endl; // 1524 = 10111110100 (7 ones)
 
     vector<char> v = {'a', 'b', 'c'};
+    printSubsets(v);
 }
